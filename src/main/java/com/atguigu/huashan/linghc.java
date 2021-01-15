f65549a372624f182cfc74590fbd0f3774dde57d
anda <80590692@qq.com>
2021-01-16 00:23:31 +0800

    增加linghcdugujiujian

---
+++ src/main/java/com/atguigu/huashan/linghc.java
@@ -0,0 +1,9 @@
+package com.atguigu.huashan;
+
+public class linghc {
+    public static void main(String[] args) {
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+    }
+}
